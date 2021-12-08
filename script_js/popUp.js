5f6a35fde67ddb25a5f1ae05d756b5b8c1cbe987
AndyWongKH <etu.wongandy@gmail.com>
2021-12-08 17:13:13 +0100

    Ajout des pop up inscription et connexion

--- script_js/popUp.js
+++ script_js/popUp.js
@@ -1,2 +1,16 @@
+// <iframe id="inscriptionPopUp" src= "inscription.html">
+// </iframe>
+function AfficherPopUP(){
+    console.log("yo")
+    let popUp_place = document.getElementById('popUp_place');
+    let inscription = document.createElement('iframe');
+    inscription.classList.add('inscriptionPopUp');
+    inscription.setAttribute('src','connexion.html');
+    inscription.setAttribute('id','iframe_connexion');
+    popUp_place.appendChild(inscription);
+}
 
-
+function FermerPopUp(){
+    let connexion_frame = window.parent.document.getElementById('iframe_connexion');
+    connexion_frame.parentNode.removeChild(window.parent.document.getElementById('iframe_connexion'));
+}

d672ad4d07af9c643476e0c5e225524a8e177128
AndyWongKH <etu.wongandy@gmail.com>
2022-04-14 03:33:32 +0200

    Modification des conditions dans modifierArmoire.php

--- modifierArmoire.php
+++ modifierArmoire.php
@@ -11,21 +11,31 @@
 
     // Suppression de toute les lignes dans la table contenir où l'id = $idProd
     $requete = ("DELETE FROM contenir WHERE id_prod = $idProd AND id_armoire = $armoire");
+
     if(mysqli_query($session, $requete) == FALSE){
         echo("Le produit n'a pas pu être supprimé de votre inventaire");
+    }else{
+        // Inserer le produit avec la nouvelle qte dans l'armoire
+        if($qte > 0){
+            $requete1 = ("INSERT INTO contenir(id_prod,id_armoire,qte_prod) VALUES($idProd,$armoire,$qte)");
+            if(mysqli_query($session , $requete1) == TRUE){
+                // Redirection vers la page utilisateur
+                echo("<script>
+                    window.location = 'utilisateur.php';
+                </script>");
+            }
+            else{
+                echo("Votre requête à échoué, veuillez réessayer");
+            };
+        }
+        else{
+            echo("<script>
+            window.location = 'utilisateur.php';
+            </script>");
+        };
     };
 
-    // Inserer le produit avec la nouvelle qte dans l'armoire
-    $requete1 = ("INSERT INTO contenir(id_prod,id_armoire,qte_prod) VALUES($idProd,$armoire,$qte)");
-    if(mysqli_query($session , $requete1) == TRUE){
-        // Redirection vers la page utilisateur
-        echo("<script>
-                window.location = 'utilisateur.php';
-            </script>");
-    }
-    else{
-        echo("echec de l'insertion");
-    };
+
 ?>
 
 
